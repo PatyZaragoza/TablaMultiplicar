7c79f8aaed36c5b06cb2a6ab364cd40b816717b6
Paty Zaragoza <patypalma878@gmail.com>
2024-10-20 21:34:47 -0600

    Primer commit - Subiendo el proyecto a GitHub

---
+++ Tabla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int tabla, i;
+
+    // Solicitar al usuario qué tabla de multiplicar desea ver
+    printf("¿Qué tabla de multiplicar deseas ver? ");
+    scanf("%d", &tabla);
+
+    // Imprimir la tabla de multiplicar seleccionada
+    printf("\nTabla del %d\n", tabla);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", tabla, i, tabla * i);
+    }
+
+    return 0;
+}
---
+++ tabla.py
@@ -0,0 +1,7 @@
+# Solicitar al usuario qué tabla de multiplicar desea ver
+tabla = int(input("¿Qué tabla de multiplicar deseas ver? "))
+
+# Imprimir la tabla de multiplicar seleccionada
+print(f"\nTabla del {tabla}")
+for i in range(1, 11):
+    print(f"{tabla} x {i} = {tabla * i}")
